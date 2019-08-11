20225cf2f4ea86e9d3441abd823f1b00a43510ff
Alexander Galaydyuk <galaydyukav@gmail.com>
2019-08-11 05:22:44 +0300

    docs: add default snippet to Vetur

---
+++ .vscode/vetur/snippets/default.vue
@@ -0,0 +1,15 @@
+<template>
+  ${0}
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ${TM_FILENAME_BASE} extends Vue {
+  ${1}
+}
+</script>
+
+<style lang="stylus">
+</style>
